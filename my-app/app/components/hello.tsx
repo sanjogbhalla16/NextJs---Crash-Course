ba6f98798b67e817273ff438eb91f37a27498e40
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2024-12-04 00:14:12 +0530

    NextJs - Crash Course

---
+++ my-app/app/components/hello.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+function Hello() {
+  console.log("I am a Client Component");
+
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default Hello;
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
+import Hello from "./components/hello";
 
 export default function Home() {
   console.log("what am i doing here");
 
   return (
-    <div>
+    <>
+      <Hello></Hello>
       <h1>Welcome to NextJs</h1>
       <Image src="/vercel.svg" alt="Vercel Logo" width={100} height={24} />
-    </div>
+    </>
   );
 }
